579ea969eafa95fe90995969e6a055381a552cbb
themsiqueira <mateussiqueiracarneiro@outlook.com>
2019-10-18 08:26:52 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+import EventOrganizeController from './app/controllers/EventOrganizerController';
+
+import CheckPayloadMiddleware from './app/middlewares/CheckPayload';
+
+const routes = new Router();
+
+routes.post(
+  '/organize/event',
+  CheckPayloadMiddleware,
+  EventOrganizeController.Organizer
+);
+
+export default routes;
