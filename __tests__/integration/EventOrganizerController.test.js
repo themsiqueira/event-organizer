65dc174614e8aefee14ad0c224dc8fafde769d5c
themsiqueira <mateus.carneiro@agrotools.com.br>
2019-12-12 15:30:03 -0300

    Tests Ajusts

--- __tests__/integration/EventOrganizerController.test.js
+++ __tests__/integration/EventOrganizerController.test.js
@@ -6,10 +6,9 @@ describe('event organizer controller', () => {
   it('Should return a schedule for a event', async () => {
     const response = await request(app)
       .post('/api/organize/event')
-      .send(data)
+      .send({ data })
       .expect(200);
 
     expect(response.body).toHaveProperty('data');
-    expect(response.body.data).toHaveLength();
   });
 });
